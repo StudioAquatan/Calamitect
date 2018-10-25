3dee74092e21bdb4f1349e85ffd4744b91359dac
shanpu <cheztomo513@gmail.com>
2018-10-25 16:14:46 +0900

    update settings
    imageを保存するため

--- src/calamitect/settings.py
+++ src/calamitect/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'boards',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- src/calamitect/urls.py
+++ src/calamitect/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+
+from src.calamitect import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('boards.urls')),
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     path('user/', include('accounts.urls')),
 ]
