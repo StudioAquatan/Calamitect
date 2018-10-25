584fac68a647ac91ec3a68aa581c0b0bd4e2a0a4
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2018-10-25 15:38:56 +0900

    ルーティングとビュー

--- src/boards/views.py
+++ src/boards/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+def index(request):
+    return render(request, 'boards/index.html')
+
+
+def create(request):
+    form = UserCreationForm()
+    return render(request, 'boards/new.html', {'form': form})
