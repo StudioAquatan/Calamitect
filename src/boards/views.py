00c65c80fdc4ca37f5f57859de9738a86bdaaeac
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2018-10-27 06:02:03 +0900

    カテゴリTOPとユーザー認証処理

--- src/boards/views.py
+++ src/boards/views.py
@@ -1,27 +1,40 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
-# from . import Article
-
+from .models import Article
 
 def index(request):
+    print(request.user.is_authenticated)
     return render(request, 'boards/index.html')
 
+def categoryTop(request):
+    category_type = request.GET['category_type']
+
+    try:
+        articles = Article.objects.filter(category_type=category_type)
+        print(vars(articles))
+    except User.DoesNotExist:
+        empty = "まだ記事が投稿されていません。"
+        return render(request, 'boards/category_top.html', {'empty': empty})
+
+    return render(request, 'boards/category_top.html', {'articles': articles})
 
 def create(request):
 
     if request.method == 'POST':
-        title = request.POST['title']
-        message = request.POST['message']
+        if request.user.is_authenticated:
+            title = request.POST['title']
+            description = request.POST['description']
+            category_type = request.POST['category_type']
 
-        user = User.objects.first()
 
-        # topic = Article.objects.create(
-        #     title=title,
-        #     board=,
-        #     starter=user
-        # )
+            Article.objects.create(
+                title=title,
+                description=description,
+                category_type=category_type,
+                user=request.user
+            )
 
-        return redirect('index')
+            return render(request, 'boards/index.html')
 
     return render(request, 'boards/new.html')
