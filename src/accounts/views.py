39087b16017c161e36f6c06a148306279ba6c501
shanpu <cheztomo513@gmail.com>
2018-10-26 14:45:15 +0900

    fix models
    urlに不備があったので修正

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 
+
 def create(request):
     form = UserCreationForm()
     return render(request, 'accounts/new.html', {'form': form})
--- src/calamitect/urls.py
+++ src/calamitect/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 from src.calamitect import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('boards.urls')),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     path('user/', include('accounts.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
