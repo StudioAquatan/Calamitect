f820bf5872f1cae0e8b7a7fc5f0f78d5b688fb8f
shanpu <cheztomo513@gmail.com>
2018-11-03 17:30:12 +0900

    update urls

--- src/boards/urls.py
+++ src/boards/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
+from rest_framework import routers
 
 from . import views
+from .views import GoodViewSet, FavoriteViewSet
 
 app_name = 'boards'
+
+router = routers.DefaultRouter()
+router.register(r'goods', GoodViewSet)
+router.register(r'favorite', FavoriteViewSet)
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('boards/new/', views.create, name='create'),
--- src/calamitect/urls.py
+++ src/calamitect/urls.py
@@ -13,17 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
 from src.calamitect import settings
+from ..boards.urls import router as board_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('boards.urls')),
     path('user/', include('accounts.urls')),
+    path('api/', include(board_router.urls))
 ]
 
 if settings.DEBUG:
