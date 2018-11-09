fd7c1e1ba8fc8114e08f246aa9ef771707255b8c
shanpu <cheztomo513@gmail.com>
2018-11-10 02:01:38 +0900

    イメージがなくても表示できるように

--- src/boards/views.py
+++ src/boards/views.py
@@ -18,6 +18,7 @@ def index(request):
     quake_info, display = get_quake_info()
 
     try:
+        article = Article.objects.get(pk=1)
         articles = Article.objects.all()
         articles = articles.order_by("created_at").reverse()
     except Article.DoesNotExist:
