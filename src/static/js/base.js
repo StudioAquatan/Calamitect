cbefa81ef93e093a058f69a71daf3397f555d581
Takuya Iida <wakatakerutmt@ezweb.ne.jp>
2018-11-09 23:20:27 +0900

    material design

--- src/static/js/base.js
+++ src/static/js/base.js
@@ -30,4 +30,18 @@
        }
    });
 
+   var my_menu_btn = document.getElementById('my_menu_btn');
+   var my_menu = document.getElementById("my_menu");
+   var my_menu_flag = 0
+
+   my_menu_btn.addEventListener('click', function() {
+       if(my_menu_flag==0){
+           my_menu.classList.remove("display-none");
+           my_menu_flag = 1;
+       }else{
+           my_menu.classList.add("display-none");
+           my_menu_flag = 0;
+       }
+   });
+
 })();
